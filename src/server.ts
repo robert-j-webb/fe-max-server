f2f148878c6ad6ae04e7d383da5433242f8c225e
Robert Webb <robertwebb@modular.com>
2025-06-30 12:09:16 -0700

    Remove user

--- src/server.ts
+++ src/server.ts
@@ -5,7 +5,6 @@ import { pino } from "pino";
 
 import { openAPIRouter } from "@/api-docs/openAPIRouter";
 import { healthCheckRouter } from "@/api/healthCheck/healthCheckRouter";
-import { userRouter } from "@/api/user/userRouter";
 import errorHandler from "@/common/middleware/errorHandler";
 import rateLimiter from "@/common/middleware/rateLimiter";
 import requestLogger from "@/common/middleware/requestLogger";
@@ -32,7 +31,6 @@ app.use(requestLogger);
 // Routes
 app.use("/health-check", healthCheckRouter);
 app.use("/max", maxRouter);
-app.use("/users", userRouter);
 
 // Swagger UI
 app.use(openAPIRouter);
