883d060b6d7b6af67d870248fdc1426949fb09d3
Austin Doolittle <austin@modular.com>
2025-09-02 09:18:41 -0700

    set user ID to ensure metrics get through

--- src/api/max/maxServeService.ts
+++ src/api/max/maxServeService.ts
@@ -85,9 +85,10 @@ class MaxServeService {
       all: true,
       env: {
         MAX_SERVE_LOGS_OTLP_LEVEL: "DEBUG",
-        MAX_SERVE_DEPLOYMENT_ID: `INTERNAL_MODULAR_CONSOLE`,
+        MAX_SERVE_DEPLOYMENT_ID: `INTERNAL_MODULAR_CONSOLE_${modelName}`,
         MAX_SERVE_METRIC_LEVEL: "DETAILED",
         MAX_SERVE_METRIC_RECORDING_METHOD: "PROCESS",
+        MODULAR_USER_ID: `INTERNAL_MODULAR_CONSOLE`,
       },
     })`${command}`;
     this.hasMaxStarted = true;
